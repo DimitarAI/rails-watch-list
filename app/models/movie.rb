6afefae7d74d84398445117c1d0ade4aec9b428f
Dimitar Petrov <petrov1501@gmail.com>
2024-02-22 11:58:13 +0100

    yes

---
+++ app/models/movie.rb
@@ -0,0 +1,17 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+
+  before_destroy :check_for_bookmarks
+
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
+
+  private
+
+  def check_for_bookmarks
+    if bookmarks.count > 0
+      errors.add(:base, "Cannot delete movie with bookmarks")
+      throw :abort
+    end
+  end
+end
