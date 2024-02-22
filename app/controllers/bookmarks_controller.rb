c2546c3e98de43ffaf420cbdfb8b2e84d72e93dc
Dimitar Petrov <petrov1501@gmail.com>
2024-02-22 17:02:19 +0100

    ye

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,22 +3,26 @@ class BookmarksController < ApplicationController
   before_action :set_bookmark, only: [:destroy]
 
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
+
     if @bookmark.save
-      redirect_to @list
+      redirect_to list_path(@list)
     else
       render :new
     end
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to @bookmark.list
+    redirect_to list_path(@bookmark.list), notice: 'Bookmark was successfully deleted.'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # get 'lists/new'
   # get 'lists/create'
   resources :lists do
-    resources :bookmarks, only: [:new, :create]
+    resources :bookmarks, only: [:new, :create,]
   end
   resources :bookmarks, only: [:destroy]
 end
